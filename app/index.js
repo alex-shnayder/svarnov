16cf823367e82bc029b49a23d82485416422903b
Alex Shnayder <alex@shnayder.pro>
2015-11-26 07:52:37 +0600

    Add catalog, improve main page markup

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var app = require('./src/app');
+var store = require('./store');
 
 module.exports = function(assets, stats, cb) {
   var pages = {
-    'index.html': app.page('main', 'common', assets)
+    'index.html': app.page('main', 'common', assets, store)
   };
   return cb(null, pages);
 };
--- app/src/app/index.js
+++ app/src/app/index.js
@@ -2,10 +2,11 @@
 
 var template = require('./template');
 
-module.exports.page = function(page, layout, assets) {
+module.exports.page = function(page, layout, assets, store) {
   return template({
     layout: layout,
     page: page,
-    assets: assets
+    assets: assets,
+    store: store
   });
 };
