086d8542fd9081b1bd1d4aaf3073ce24ab45a431
Alex Shnayder <alex@shnayder.pro>
2015-11-26 07:47:17 +0600

    Store module

---
+++ app/store.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var fs = require('fs');
+var Path = require('path');
+
+module.exports.pages = function pages() {
+  return require('./content/pages.json');
+};
+
+module.exports.categories = function categories() {
+  return require('./content/categories.json');
+};
+
+module.exports.items = function items() {
+  var categories = this.categories();
+  return Object.keys(categories).reduce(function(items, categoryId) {
+    var categoryPath = 'content/items/' + categoryId;
+
+    items[categoryId] = [];
+
+    try {
+      items[categoryId] = fs.readdirSync(Path.join(__dirname, categoryPath)).map(function(itemId) {
+        var item = require('./' + categoryPath + '/' + itemId + '/index');
+        item.category = categories[categoryId];
+        item.fullLink = item.category.link + '/' + item.link;
+        return item;
+      });
+    } catch(err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+
+    return items;
+  }, {});
+};
