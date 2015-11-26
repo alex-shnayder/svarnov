ecdc2cb964ec9cddf57a48957e243011748ef654
Alex Shnayder <alex@shnayder.pro>
2015-11-26 08:18:38 +0600

    Change the way endpoints are generated

--- app/index.js
+++ app/index.js
@@ -4,8 +4,30 @@ var app = require('./src/app');
 var store = require('./store');
 
 module.exports = function(assets, stats, cb) {
-  var pages = {
-    'index.html': app.page('main', 'common', assets, store)
-  };
-  return cb(null, pages);
+  var pages = store.pages();
+  var categories = store.categories();
+  var endpoints = {};
+
+  Object.keys(pages).forEach(function(pageId) {
+    var page = pages[pageId];
+    endpoints[(page.link || 'index') + '.html'] = app.page({
+      page: pageId,
+      layout: 'common',
+      assets: assets,
+      store: store
+    });
+  });
+
+  Object.keys(categories).forEach(function(categoryId) {
+    var category = categories[categoryId];
+    endpoints[category.link + '.html'] = app.page({
+      page: 'category',
+      layout: 'common',
+      assets: assets,
+      store: store,
+      category: categoryId
+    });
+  });
+
+  return cb(null, endpoints);
 };
--- app/src/app/index.js
+++ app/src/app/index.js
@@ -2,11 +2,6 @@
 
 var template = require('./template');
 
-module.exports.page = function(page, layout, assets, store) {
-  return template({
-    layout: layout,
-    page: page,
-    assets: assets,
-    store: store
-  });
+module.exports.page = function(locals) {
+  return template(locals);
 };
