c557fc058287c94ae3b173b1c39fb26b388defda
Alex Shnayder <alex@shnayder.pro>
2015-12-04 02:21:43 +0600

    Lots of updates

--- app/store.js
+++ app/store.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var Path = require('path');
 
 module.exports.pages = function pages() {
-  return require('./content/pages.json');
+  return require('./content/pages');
 };
 
 module.exports.categories = function categories() {
-  return require('./content/categories.json');
+  return require('./content/categories');
 };
 
 module.exports.items = function items() {
