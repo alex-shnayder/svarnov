bf090fbe010df9a45b35a970b71ad8381c8381de
Alex Shnayder <alex@shnayder.pro>
2015-12-04 02:20:03 +0600

    Basic content

---
+++ app/content/categories.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  inverters: {
+    link: 'инверторы',
+    name: 'Инверторы',
+    itemName: 'Инвертор'
+  },
+  semiautoDevices: {
+    link: 'полуавтоматы',
+    name: 'Полуавтоматы',
+    itemName: 'Полуавтомат'
+  },
+  argonDevices: {
+    link: 'аргонники',
+    name: 'Аргонники',
+    itemName: 'Аргонник'
+  },
+  plasmicCutters: {
+    link: 'плазморезы',
+    name: 'Плазморезы',
+    itemName: 'Плазморез'
+  }
+};
---
+++ app/content/pages.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  main: {
+    link: '',
+    name: 'Главная'
+  },
+  contacts: {
+    link: 'контакты',
+    name: 'Контакты'
+  }
+};
