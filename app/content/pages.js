e3ba88a899d2dcd3bc7a14ce975b765135998fc6
Alex Shnayder <alex@shnayder.pro>
2015-12-31 03:41:00 +0600

    Delivery page

--- app/content/pages.js
+++ app/content/pages.js
@@ -5,6 +5,10 @@ module.exports = {
     link: '',
     name: 'Главная'
   },
+  delivery: {
+    link: 'доставка-и-оплата',
+    name: 'Доставка и оплата'
+  },
   contacts: {
     link: 'контакты',
     name: 'Контакты'
